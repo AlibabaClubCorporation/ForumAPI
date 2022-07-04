5e818d59b392f92b9b32ac5045b1b8b95f29476f
Ilia Ivko <mrloking11@gmail.com>
2022-07-04 17:29:57 +0200

    Add app to settings. Test view. Create initial models

--- FAPI/forum/models.py
+++ FAPI/forum/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Themes(models.Model):
+    """
+        Класс модели темы
+    """
+
+    title = models.CharField( max_length = 256, unique = True )
+
+
+class Phors(models.Model):
+    """
+        Класс модели фора
+    """
+
+    title = models.CharField( max_length = 256, unique = True )
+    description = models.TextField( max_length = 4096, )
+
+    date_of_creation = models.DateTimeField( auto_now_add = True, )
+
+    theme = models.ForeignKey( Themes, on_delete = models.CASCADE, related_name = 'phors' )
+    creator = models.ForeignKey( User, on_delete = models.SET_NULL, null = True, related_name = 'phors' )
+
+
+class Answers(models.Model):
+    """
+        Класс модели ответа
+    """
+
+    content = models.TextField( max_length = 4096, )
+
+    date_of_creation = models.DateTimeField( auto_now_add = True, )
+
+    is_correct = models.BooleanField( default = False, )
+
+    phor = models.ForeignKey( Phors, on_delete = models.CASCADE, related_name = 'answers' )
+    creator = models.ForeignKey( User, on_delete = models.CASCADE, related_name = 'answers' )
--- FAPI/forum/views.py
+++ FAPI/forum/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def v(request):
+    return HttpResponse('dsad')
