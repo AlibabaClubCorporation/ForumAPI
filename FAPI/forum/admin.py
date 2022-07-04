ea71225879417ed8d20290aef5b722c2b43e1c9d
Ilia Ivko <mrloking11@gmail.com>
2022-07-04 18:48:15 +0200

    New field "slug" in model Phors and Themes. Small change in Models. Register models in admin.py.

--- FAPI/forum/admin.py
+++ FAPI/forum/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PhorInline( admin.StackedInline ):
+    model = Phors 
+    extra = 0
+
+    readonly_fields = ( 'date_of_creation', )
+    fieldsets = (
+        ( None, {
+            'fields' : ( ( 'creator', 'theme' ), 'title', 'date_of_creation', ),
+        }),
+
+        ( 'Description', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( 'description', )
+        }),
+
+        ( 'Options', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( 'slug', )
+        })
+    )
+
+class AnswerInline( admin.StackedInline ):
+    model = Answers
+    extra = 0
+
+    readonly_fields = ( 'date_of_creation', )
+
+
+@admin.register( Themes )
+class ThemesAdmin( admin.ModelAdmin ):
+    search_fields = ( 'title', )
+    list_filter = ( 'title', )
+    list_display = ( 'title', )
+    list_display_links = ( 'title', )
+
+    prepopulated_fields = { 'slug' : ( 'title', ) }
+
+    inlines = [PhorInline, ]
+
+    save_on_top = True
+
+@admin.register( Phors )
+class PhorsAdmin( admin.ModelAdmin ):
+    list_display = ( 'title', 'date_of_creation', 'theme', 'creator' )
+    list_display_links = ( 'title', )
+    list_editable = ( 'theme', )
+    list_filter = ( 'title', 'date_of_creation', )
+    search_fields = ( 'title', )
+    readonly_fields = ( 'date_of_creation', )
+    
+    fieldsets = (
+        ( None, {
+            'fields' : ( ( 'creator', 'theme' ), 'title', 'date_of_creation', ),
+        }),
+
+        ( 'Description', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( 'description', )
+        }),
+
+        ( 'Options', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( 'slug', )
+        })
+    )
+
+    prepopulated_fields = { 'slug' : ( 'title', ) }
+
+    inlines = [AnswerInline, ]
+
+    save_on_top = True
+    save_as = True
+
+
--- FAPI/forum/models.py
+++ FAPI/forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 
@@ -8,7 +9,15 @@ class Themes(models.Model):
         Класс модели темы
     """
 
-    title = models.CharField( max_length = 256, unique = True )
+    title = models.CharField( max_length = 128, unique = True, )
+
+    slug = models.SlugField( max_length = 256, unique = True, db_index = True, )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse( 'theme', kwargs = { 'theme_slug' : self.slug } )
 
 
 class Phors(models.Model):
@@ -16,7 +25,7 @@ class Phors(models.Model):
         Класс модели фора
     """
 
-    title = models.CharField( max_length = 256, unique = True )
+    title = models.CharField( max_length = 128, unique = True )
     description = models.TextField( max_length = 4096, )
 
     date_of_creation = models.DateTimeField( auto_now_add = True, )
@@ -24,8 +33,16 @@ class Phors(models.Model):
     theme = models.ForeignKey( Themes, on_delete = models.CASCADE, related_name = 'phors' )
     creator = models.ForeignKey( User, on_delete = models.SET_NULL, null = True, related_name = 'phors' )
 
+    slug = models.SlugField( max_length = 256, unique = True, db_index = True, )
+
     class Meta:
         ordering = [ 'date_of_creation', 'title' ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse( 'phor', kwargs = { 'phor_slug' : self.slug } )
 
 class Answers(models.Model):
     """
