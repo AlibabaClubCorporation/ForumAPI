d43564b35a28ffc08991a3575affc3113c6f990b
Ilia Ivko <mrloking11@gmail.com>
2022-07-07 14:39:59 +0200

    Add name to paths in urls.py.

--- FAPI/forum/urls.py
+++ FAPI/forum/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 # Для своего удобства я не стал указывать в маршруте лишние символы наподобии: api/v1.23/
 urlpatterns = [
-    path( 'themes/', views.ListThemeAPIView.as_view() ),
-    path( 'themes/create/', views.CreateThemeAPIView.as_view() ),
+    path( 'themes/', views.ListThemeAPIView.as_view(), name = 'themes' ),
+    path( 'themes/create/', views.CreateThemeAPIView.as_view(), name = 'theme_create' ),
     path( 'themes/<slug:slug_of_theme>/', views.DetailThemeAPIView.as_view(), name = 'theme' ),
 
-    path( 'phors/create/', views.CreatePhorAPIView.as_view() ),
+    path( 'phor/create/', views.CreatePhorAPIView.as_view(), name = 'phor_create' ),
     path( 'themes/<slug:slug_of_theme>/<slug:slug_of_phor>/', views.DetailPhorAPIView.as_view(), name = 'phor' ),
+
+    path( 'answer/create/', views.CreateAnswerAPIView.as_view(), name = 'answer_create' ),
 ]
